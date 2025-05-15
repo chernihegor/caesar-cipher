05210ec2f902ed3a644e78ebf2dbf2afcbc4c4ce
chernihegor <94792035+chernihegor@users.noreply.github.com>
2025-05-15 23:27:12 +0300

    Добалена ф-ция шифрования методом Цезаря

---
+++ caesar-cipher.py
@@ -0,0 +1,19 @@
+def encrypt(text: str, shift: int) -> str:
+    encrypted = ""
+    for char in text:
+        if char.isalpha():
+            shifted = ord(char) + shift
+            if char.islower():
+                if shifted > ord('z'):
+                    shifted -= 26
+                elif shifted < ord('a'):
+                    shifted += 26
+            elif char.isupper():
+                if shifted > ord('Z'):
+                    shifted -= 26
+                elif shifted < ord('A'):
+                    shifted += 26
+            encrypted += chr(shifted)
+        else:
+            encrypted += char
+    return encrypted
